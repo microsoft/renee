a94ed3a52d69fc3d0de02c0a18658ea84397f5eb
jviditmsr <jainvidit@microsoft.com>
2023-09-28 05:24:11 +0000

    Add 2.0 hyperparams for LF datasets. Update install2. Reproduce on DGX-2

--- install2.sh
+++ install2.sh
@@ -5,12 +5,13 @@ conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvi
 
 pip install transformers
 pip install scipy
-pip install numba
 pip install pandas
 pip install cython
 pip install scikit-learn
 pip install sentence-transformers
 pip install seaborn
+pip install numpy==1.21.0
+pip install numba==0.56
 
 git clone https://github.com/kunaldahiya/pyxclib
 cd pyxclib
@@ -19,8 +20,9 @@ cd ..
 # rm -rf pyxclib
 
 ## Need only for apex optimizers
-git clone https://github.com/NVIDIA/apex
+git clone https://github.com/NVIDIA/apex 
 cd apex
+git checkout 5c9625cfed681d4c96a0ca4406ea6b1b08c78164
 pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 cd ..
 # rm -rf apex
